5fa723ec9a9e1125482d81471891c5c3909a6a8b
kayla <jadefoxx01@gmail.com>
2024-03-14 19:20:46 -0400

    Close navbar on link click mobile

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
-const checkbox = document.getElementById('check');
+const navWelcome = document.getElementById('welcome-item');
+const navAbout = document.getElementById('about-item');
+const navProjects = document.getElementById('projects-item');
+const navContact = document.getElementById('contact-item');
+const checkbox = document.getElementById('check')
 
-checkbox.addEventListener("click", uncheck);
+navWelcome.addEventListener("click", uncheck);
+navAbout.addEventListener("click", uncheck);
+navProjects.addEventListener("click", uncheck);
+navContact.addEventListener("click", uncheck);
 
 function uncheck() {
-    
+    if(checkbox.checked == true) {
+        checkbox.checked = false;
+    }
+}
+
+uncheck();
+
+if(checkbox.checked == true) {
+    //hide scrollbar
 }
