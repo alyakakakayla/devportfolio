e472841f9110aa610c208543313924754f7c3cb5
kayla <jadefoxx01@gmail.com>
2024-06-06 18:02:45 -0400

    correct js variables for navbar

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const navWelcome = document.getElementById('welcome-item');
 const navAbout = document.getElementById('about-item');
-const navProjects = document.getElementById('projects-item');
+const navWork = document.getElementById('work-item');
 const navContact = document.getElementById('contact-item');
 const checkbox = document.getElementById('check')
 
 navWelcome.addEventListener("click", uncheck);
 navAbout.addEventListener("click", uncheck);
-navProjects.addEventListener("click", uncheck);
+navWork.addEventListener("click", uncheck);
 navContact.addEventListener("click", uncheck);
 
 function uncheck() {
